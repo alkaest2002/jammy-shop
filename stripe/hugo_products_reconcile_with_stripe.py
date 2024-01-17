f129784db598932158c2e7478c0579956704d33a
Pierpaolo Calanna <p.calanna@gmail.com>
2024-01-17 17:54:43 +0100

    bugfixes

--- stripe/hugo_products_reconcile_with_stripe.py
+++ stripe/hugo_products_reconcile_with_stripe.py
@@ -3,18 +3,30 @@ import frontmatter
 from io import BytesIO
 from pathlib import Path
 
+# open stripe product json
 with open("./stripe_products.json", "r") as fin:
   stripe_prodcuts = json.load(fin)
 
+# loop through products
 for product_sku, product in stripe_prodcuts.items():
+
+  # build hugo product markdown file
   hugo_md_file = Path("../hugo/content/products") / product_sku / "index.md"
   
+  # open hugo product markdown file
   with open(hugo_md_file, "r") as fin:
+    # load data from file
     hugo_md = frontmatter.load(fin)
+    # update hugo markdown data with stripe data
     hugo_md.metadata["stripe_price_id"] = product["default_price"]
     hugo_md.metadata["stripe_product_id"] = product["id"]
+    
+    # init buffer
     buffer = BytesIO()
+
+    # dump hugo product to buffer
     frontmatter.dump(hugo_md, buffer)
   
+  # save buffer to disk
   with open(hugo_md_file, "wb") as fout:
     fout.write(buffer.getbuffer())
--- stripe/stripe_products_get.py
+++ stripe/stripe_products_get.py
@@ -7,18 +7,29 @@ load_dotenv()
 stripe.api_key = os.getenv("STRIPE_API_KEY")
 
 def load_products(starting_after = None, products = {}, limit = 100):
+  
+  # get stripe products using stripe api
   prouducts_from_stripe = stripe.Product.list(limit = limit, starting_after = starting_after)
+  
   # loop through products
   for product in prouducts_from_stripe.data:
+    # add price from stripe defualt price
     product["price"] = stripe.Price.retrieve(product["default_price"])["unit_amount"] / 100
+    # add sku to metadata
     products[product.metadata.sku] = product
+
+  # if there are other products to load
   if prouducts_from_stripe.has_more:
+    # go recursive
     return load_products(prouducts_from_stripe.data[-1], products, limit)
+  
+  # return json products
   return  products
 
+# call fn
 products = load_products(None, {}, 5)
 
-# write json objectxw
+# write json object
 with open("stripe_products.json", "wb") as fout:
   encoded_data = json.dumps(products, indent=2).encode('utf-8')
   fout.write(encoded_data)
