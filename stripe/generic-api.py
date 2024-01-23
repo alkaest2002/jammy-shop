38ddf5078d4dfce15bb2ddc81378a67464de82cc
Pierpaolo Calanna <p.calanna@gmail.com>
2024-01-23 13:42:55 +0100

    updating thank you page

--- stripe/generic-api.py
+++ stripe/generic-api.py
@@ -1,15 +1,20 @@
 
 import os
+import json
 import stripe
 from dotenv import load_dotenv
 
 load_dotenv()
 stripe.api_key = os.getenv("STRIPE_API_KEY")
 
-stripe.ShippingRate.modify(
-  "shr_1ObMDrBC2mcEPes8mFs1vb0z",
-  fixed_amount = { 
-    "currency_options": {
-      "usd": { "amount": 300, "tax_behavior": "exclusive" }
-  } }
-)
+
+# open stripe product json
+with open("./stripe_products.json", "r") as fin:
+  stripe_prodcuts = json.load(fin)
+
+
+for product_sku, product in stripe_prodcuts.items():
+  stripe.Product.modify(
+    product["id"],
+    tax_code="txr_1ObMsPIq1JpnhegE62sSe7s5",
+  )
